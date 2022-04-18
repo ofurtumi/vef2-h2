adbfc8d9dc2f2c10fef9cbd596bd6635e3112928
Þorvaldur Tumi Baldursson <thorvaldur.tumi@gmail.com>
2022-04-18 23:19:59 +0000

    allt er í fokki ekkert virkar ég er að brjálast

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,22 +1,40 @@
 import { Menu } from "../types";
-import styles from '../styles/main.module.css'
+import styles from "../styles/main.module.css";
+import { useState } from "react";
 
-const Menu = (props: { menu: Menu }) => {
-  console.log('props.menu --> ', props.menu)
+const Menu = (props: { menu: Menu, orderArray: Array<number> }) => {
+  // const [orders, setOrders] = useState(Array.apply(null, Array(props.menu.items.length)).map(function (x=0) { return x; }));
+  const [orders, setOrders] = useState(props.orderArray);
+  // ! okok svo þetta "virkar" en ehv týpu vandamál, laga þetta setja any eða ehv þannig
+  // todo: setja ákv týpu á þetta eða bara leysa öðruvísi
+  const sum = orders.reduce((acc, item) => acc + item, 0);
+  
   return (
     <div className={styles.prison}>
-      {props.menu.items.map((item)=> {
+      {props.menu.items.map((item, i) => {
         return (
           <div className={styles.cell}>
             <h1>{item.title}</h1>
             <p>{item.description}</p>
             <h2>{item.price}</h2>
+            {/* // ! bruh hvernig í fokkanum set ég any á þetta shit */}
+            <h3>{orders[i]}</h3>
             {/* <Image src={item.image} layout='fill'></Image> */}
+            <button
+              onClick={() => {
+                const ordersCopy = [...orders];
+                ordersCopy[i] += 1;
+                setOrders(ordersCopy);
+              }}
+            >
+              Bæta í körfu
+            </button>
           </div>
-        )
+        );
       })}
+      <p>summa: {sum}</p>
     </div>
-  )
+  );
 };
 
 export async function getServerSideProps() {
@@ -26,8 +44,13 @@ export async function getServerSideProps() {
   let menu = null;
   if (rawMenu.ok) menu = await rawMenu.json();
 
+  let orderArray = Array<number>(menu.items.length)
+  for (let i = 0; i < orderArray.length; i++) {
+    orderArray[i] = 0;
+  }
+
   return {
-    props: { menu }, // will be passed to the page component as props
+    props: { menu, orderArray }, // will be passed to the page component as props
   };
 }
 
