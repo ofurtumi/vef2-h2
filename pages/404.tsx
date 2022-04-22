f1403219a0100401ea0c89848732408b211b3d57
Gunnarbjo <gbt6@hi.is>
2022-04-22 09:37:15 +0000

    Custom 404 page
    Ekki viss hvort að hún eigi að vera í typescript en hef hana þannig þar til annað kemur í ljós

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+export default function Custom404() {
+  return (
+    <div className={styles.container}>
+      
+    <div>
+    <h1>Oops, something went wrong</h1>
+    <p><a href="..">Go back to front page</a></p>
+  </div>
+  </div>
+  )
+}
